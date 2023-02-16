a31b85c5e3f29819b3bbcb0fbe4ad8cf94bce119
mohammadsalarvand403 <mmdslv7707@gmail.com>
2023-02-17 01:24:39 +0330

    securing pages on Client-Side

--- pages/Profail/profail.jsx
+++ pages/Profail/profail.jsx
@@ -1,9 +1,23 @@
 import Layout from "@/containers/Layout";
-
+import {signIn,useSession} from "next-auth/react"
 const Profail = () => {
+    const {data:session,status}=useSession({
+        required:true,
+        onUnauthenticated(){
+            signIn()
+        }
+    });
+
+    if (status === "loading") {
+        <div>
+            <p>Loading...</p>
+        </div>
+    }
     return ( 
         <Layout>
-            <h1>profail page</h1>
+            {session &&(
+                <h1>{session.user.name}, wellcome profail page</h1>
+            )}
         </Layout>
      );
 }
