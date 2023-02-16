af932a3bf8b5901237b200184b6286dbde3f7191
mohammadsalarvand403 <mmdslv7707@gmail.com>
2023-02-17 00:31:49 +0330

    client-Side Authentiaction

--- containers/Layout/Header.jsx
+++ containers/Layout/Header.jsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
-import {signIn , signOut} from "next-auth/react"
+import {signIn , signOut ,useSession} from "next-auth/react"
 const Header = () => {
+ const {data:session,status}=useSession();
     return (
         <nav className=" flex justify-between font-bold text-center py-3 px-3 border-b-2">
         <h1 className="">
          <a href="#">Todo List App</a>
         </h1>
-        <ul className="flex items-center gap-x-6 ">
+        <ul className={`flex items-center gap-x-6 ${status ==="loading" && !session ? "opacity-0":"opacity-100"}`}>
         <li>
           <Link href={"/"}>
           Home
@@ -22,12 +23,16 @@ const Header = () => {
           profail
           </Link>
          </li>
+         {!session &&status !== "loading" &&(
         <li>
-          <button onClick={()=>signIn('github')}>signin</button>
+           <button onClick={()=>signIn('github')}>Sign in</button>
         </li>
+        )}
+        {session && (
         <li>
-        <button onClick={()=>signOut('github')}>signout</button>
+         <button onClick={()=>signOut()}>Sign out</button>
         </li>
+        )}
         
         </ul>
        </nav>
