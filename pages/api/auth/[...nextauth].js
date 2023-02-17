33b6f3314b2eb68b7ea0427f968864025d9c920f
mohammadsalarvand403 <mmdslv7707@gmail.com>
2023-02-17 23:01:30 +0330

    adding access-token to user session

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,4 +12,27 @@ export default nextAuth({
       
     ],
     adapter: MongoDBAdapter(clientPromise),
+    session:{
+      strategy:"jwt"
+    },
+    jwt:{
+      secret:process.env.SECRET_JWT
+    },
+    callbacks: {
+      async session({ session, user, token }) {
+        session.user.id=token.id;
+        session.accessToken = token.accessToken;
+        return session;
+      },
+      async jwt({ token, user, account, profile, isNewUser }) {
+        if (user) {
+          user.id=user.id
+        }
+        if (account) {
+          token.accessToken = account.access_token
+        }
+        return token
+      }
+
+  }
 })
